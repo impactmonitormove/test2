93dee804f581fc94ffddf90b84d7f13743bcaf0f
impactmonitormove <impactmonitor.move@gmail.com>
2026-06-09 18:53:43 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+/*
+  Vul hier de Firebase-configuratie van de web-app in.
+  Zolang dit op null staat, blijft BeweegImpact in de lokale demomodus werken.
+*/
+window.PILOT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyClU8Es5nX39RLZxy5uu_QXUY4mwqqLOTM",
+  authDomain: "impactmonitor-77483.firebaseapp.com",
+  projectId: "impactmonitor-77483",
+  storageBucket: "impactmonitor-77483.firebasestorage.app",
+  messagingSenderId: "570891051968",
+  appId: "1:570891051968:web:df180a25ac9fe7ffef5e25"
+};
+
+/*
+window.PILOT_FIREBASE_CONFIG = {
+  apiKey: "VUL_HIER_IN",
+  authDomain: "VUL_HIER_IN.firebaseapp.com",
+  projectId: "VUL_HIER_IN",
+  storageBucket: "VUL_HIER_IN.appspot.com",
+  messagingSenderId: "VUL_HIER_IN",
+  appId: "VUL_HIER_IN"
+};
+*/
